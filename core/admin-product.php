dd24ebd90cefa1d2d6ab2d324f0a965137a06f67
Bousbih <fatoumabousbih@yahoo.fr>
2023-05-15 12:46:49 +0200

    adminchange

--- core/admin-product.php
+++ core/admin-product.php
@@ -1,26 +1,47 @@
 <?php
-//on annalyse la valeur de la variable action pour savoir si ont dois ajouter un nouveau produit ou si ont modififie in produit existant 
-//ou encore supprimer un produit
+// On analyse la valeur de la variable action
+// pour savoir si on doit ajouter un nouveau produit
+// ou si on doit modifier un produit existant
+// ou encore si on doit supprimer un produit
 switch($_POST["action"]){
     case "new":
-    addProduct();
-    break;
-
+        addProduct();
+        break;
 }
-//fonction pour ajouter un produit
-function addproduct (){
-    //1-connection à la bdd
-   require_once '../connexion.php' ;
-    //ecrire une requête sql
-    $nom=trim(strip_tags(addslashes($_POST['nom'])));
-    //on vérifie si l'utlisateur  a saissi une description
-    if (isset($_POST['description'])) {
-        $description= trim(strip_tags(addslashes($_POST['description'])));
-
-    } else{
-        $description='';
+// Fonction pour ajouter un produit
+function addProduct(){
+    // 1 - Connexion à la BDD
+    require_once '../connexion.php';
+    // 2 - Ecrire une requête SQL
+    $nom  = trim(strip_tags(addslashes($_POST['nom'])));
+    // On vérifie si l'utilisateur a saisi une description
+    if(isset($_POST['description'])){
+        $description = trim(strip_tags(addslashes($_POST['description'])));
+    } else {
+        $description = '';
     }
-   $prix = $_POST['prix'];
-  $sql = 'INSERT INTO product (nom, description, prix) VALUES ("' . $nom . '", "' . $description . '", ' . $prix . ')';
-   echo $sql;
+    $prix = $_POST['prix'];
+    // Prise en charge du fichier image
+    // On vérifie si l'utilisateur a bien sélectionné un fichier
+    if(isset($_FILES['image'])){
+        // print_r($_FILES['image']);
+        // die();
+        // On vérifie si le fichier a bien été uploadé
+        if($_FILES['image']['error'] == 0){
+            // On récupère le nom du fichier uploadé
+            $nomFichier = $_FILES['image']['name'];
+            // On passe le nom de l'image en minuscule et de supprime les espaces
+            $nomFichier = strtolower(str_replace(' ', '-', $nomFichier));
+            // On rend unique le nom de l'image avec le timestamp
+            $nomFichier = time().'-'.$nomFichier;
+            // On déplace le fichier du dossier temporaire (du serveur) vers le dossier images
+            move_uploaded_file($_FILES['image']['tmp_name'], '../images/'.$nomFichier);
+        }
+    }else{
+        $nomFichier = '';
+    }
+    $sql = 'INSERT INTO product (nom, description, prix, image_name) VALUES ("'.$nom.'", "'.$description.'", '.$prix.', "'.$nomFichier.'")';
+    //echo $sql;
+    // 3 - Exécuter la requête SQL
+    mysqli_query($connexion, $sql);
 }
